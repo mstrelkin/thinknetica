7dbab9f9a9e4d6e3c5916f62b320481d2a6fdf14
mstrelkin <mstrelkin@mail.ru>
2020-10-22 21:05:43 +0300

    task1

---
+++ lesson1/task1.rb
@@ -0,0 +1,13 @@
+puts "Как тебя зовут?"
+name = gets.chomp
+name.capitalize!
+puts "Укажи свой рост:"
+height = gets.chomp
+ideal_height = (height.to_i - 110) * 1.15
+
+if ideal_height >= 0
+	puts "#{name}, ваш идеальный вес #{ideal_height.round(2)} кг."
+else
+	puts "Ваш вес уже оптимальный."
+end
+
