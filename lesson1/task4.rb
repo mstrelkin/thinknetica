ed1d5c653fa52b8e4ef0650aa6509cab76711ffe
mstrelkin <mstrelkin@mail.ru>
2020-10-22 22:41:31 +0300

    Квадратное уравнение

---
+++ lesson1/task4.rb
@@ -0,0 +1,24 @@
+puts "Введите число 1:"
+a = gets.chomp.to_i
+puts "Введите число 2:"
+b = gets.chomp.to_i
+puts "Введите число 3:"
+c = gets.chomp.to_i
+
+disc = (b**2 - 4 * a * c)
+
+if disc > 0
+	c = Math.sqrt(disc)
+	puts "Дискриминант равен #{disc}"
+	puts "Корень 1 равен #{ (-b + c) / (2 * a)}"
+	puts "Корень 2 равен #{ (-b - c) / (2 * a)}"
+
+elsif disc == 0
+	puts "Дискриминант равен #{disc}"
+	puts "Корень равен #{-b / (2 * a)}"
+
+
+else
+	puts "Дискриминант равен #{disc}, корней нет."
+
+end
