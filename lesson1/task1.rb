e8984c80e6b92e9133054cd306f4a67b1096b892
mstrelkin <60700906+mstrelkin@users.noreply.github.com>
2020-10-23 12:49:43 +0300

    Update task1.rb

--- lesson1/task1.rb
+++ lesson1/task1.rb
@@ -6,7 +6,7 @@ height = gets.chomp
 ideal_height = (height.to_f - 110) * 1.15
 
 if ideal_height >= 0
-	puts "#{name}, ваш идеальный вес #{ideal_height.round(2)} кг."
+  puts "#{name}, ваш идеальный вес #{ideal_height.round(2)} кг."
 else
-	puts "Ваш вес уже оптимальный."
+  puts "Ваш вес уже оптимальный."
 end
