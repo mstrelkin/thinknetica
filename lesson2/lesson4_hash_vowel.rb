7529b958cc0baee9dadc105a6af41ebfda3c222a
mstrelkin <60700906+mstrelkin@users.noreply.github.com>
2020-12-11 18:34:17 +0300

    Исправление Tab вручную на Github
    Push не исправил

--- lesson2/lesson4_hash_vowel.rb
+++ lesson2/lesson4_hash_vowel.rb
@@ -3,7 +3,7 @@ hash = {}
 array = ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
 
 ('а'..'я').each.with_index(1) do |letter, index|
-	array.each { |vowel| hash[vowel] = index if letter == vowel }
+  array.each { |vowel| hash[vowel] = index if letter == vowel }
 end
 
 puts hash
