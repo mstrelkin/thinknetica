cf4607641b6dd5e4222c34ce98bba143a0479db7
mstrelkin <mstrelkin@mail.ru>
2020-12-11 17:29:45 +0300

    Исправление замечаний

--- lesson2/lesson1_month.rb
+++ lesson2/lesson1_month.rb
@@ -1,3 +1,5 @@
-mounths = {jan: 31, feb: 29, mar: 31, apr: 30, may: 31, june: 30, july: 31, aug: 31, sept: 30, nov: 30, dec: 31}
+mounths = { jan: 31, feb: 29, mar: 31, apr: 30, may: 31, june: 30, july: 31,
+aug: 31, sept: 30, nov: 30, dec: 31 }
 
-mounths.each {|a, b| puts a if b == 30}
+mounths.each { |a, b| puts a if b == 30 }
+
