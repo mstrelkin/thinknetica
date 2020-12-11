a1ea2d2b90eeece8c65d35d15afdd59a22c4b1cd
mstrelkin <mstrelkin@mail.ru>
2020-12-11 17:54:51 +0300

    Исправление ошибок

--- lesson2/lesson4_hash_vowel.rb
+++ lesson2/lesson4_hash_vowel.rb
@@ -2,8 +2,8 @@ hash = {}
 
 array = ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
 
-('а'..'я').each_with_index do |letter, index|
-	array.each {|vowel| hash[vowel] = index + 1 if letter == vowel }
+('а'..'я').each.with_index(1) do |letter, index|
+	array.each { |vowel| hash[vowel] = index if letter == vowel }
 end
 
 puts hash
