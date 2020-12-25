31c9f9feb6ff3e46ffe8679dee79d4131afd6093
mstrelkin <mstrelkin@mail.ru>
2020-12-26 01:34:51 +0300

    Основы ООП в Ruby часть 2

---
+++ lesson3/station.rb
@@ -0,0 +1,30 @@
+class Station
+
+  attr_reader :name_station, :trains
+
+  def initialize(name_station)
+    @name_station = name_station
+    @trains = []
+  end
+
+  def add_train (train)
+    @trains << train
+  end
+
+  def delete_train (train)
+    @trains.delete(train)
+  end
+
+  def type_train (type)
+    @trains.each do |train|
+      if train.type == type
+        puts train
+      end
+    end
+  end
+
+  def train_on_station
+    @trains
+  end
+
+end
--- training/car.rb
+++ training/car.rb
@@ -1,11 +1,10 @@
 class Car
   attr_accessor :speed
-
   attr_reader :engine_volume
 
   def initialize(speed = 0, engine_volume)
     @speed = speed
-    @engine_volume  = engine_volume
+    @engine_volume = engine_volume
   end
 
   def start_engine
