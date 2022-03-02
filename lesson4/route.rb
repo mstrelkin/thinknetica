effd546686c6ff3b8210e3ebc817f5255217fc65
mstrelkin <mstrelkin@mail.ru>
2022-03-02 23:18:34 +0300

    обновление lesson3

--- lesson4/route.rb
+++ lesson4/route.rb
@@ -1,16 +1,16 @@
-class Route
+class Route #Создание класса "Маршрут"
 
-  attr_reader :stations
+  attr_reader :stations #создание геттера для переменной stations (возвращение текущего значения)
 
-  def initialize(beginning_station, end_station)
+  def initialize(beginning_station, end_station) #метод конструктор, инициализация объекта с заданными локальными аргументами метода
     @stations = [beginning_station, end_station]
   end
 
-  def add_station (station)
-    @stations.insert(-2, station)
+  def add_station (station) #метод добавления промежуточной станции перед последней
+    @stations.insert(-2, station) #вставка элемента b по индексу a
   end
 
-  def delete_station (station)
+  def delete_station (station) #метод удаления станции из маршрута
     @stations.delete(station)
   end
 
--- lesson4/station.rb
+++ lesson4/station.rb
@@ -1,22 +1,22 @@
-class Station
+class Station #создали класс "Станция"
 
-  attr_reader :name_station, :trains
+  attr_reader :name_station, :trains #создание геттера для переменных
 
-  def initialize(name_station)
-    @name_station = name_station
-    @trains = []
+  def initialize(name_station) #создаем метод c начальными значениями класса "название станции"
+    @name_station = name_station #Название станции
+    @trains = [] #поезда на станции (имена поездов)
   end
 
-  def add_train (train)
+  def add_train (train) #создаем метод добавления поезда на станцию
     @trains << train
   end
 
-  def delete_train (train)
+  def delete_train (train) #метод удаления поезда со станции
     @trains.delete(train)
   end
 
-  def type_train (type)
-    @trains.each do |train|
+  def type_train (type) #метод вывода списка поездов на станции по типу поезда
+    @trains.each do |train| 
       if train.type == type
         puts train
       end
